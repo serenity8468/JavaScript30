6d2d7f236a7b4876809a111f7b67196755f2a662
Kathleen Tran <kathleen1.tran@gmail.com>
2021-05-27 19:19:27 -0500

    hooked up play and pause button

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -16,6 +16,21 @@ function togglePlay() {
 	} else {
 		video.pause();
 	}
+
+	// another way to write
+	// const method = video.paused ? 'play' : 'pause'
+	// video[method]()
+}
+
+function updateButton() {
+	const icon = this.paused ? '►' : '❚ ❚';
+	console.log(icon);
+	toggle.textContent = icon;
 }
 
 /* Hook up event listeners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
